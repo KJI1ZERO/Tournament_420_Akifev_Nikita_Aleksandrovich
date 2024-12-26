c246416a453d2eeb3150ee53abc7abe121032be5
badnikitosik55@gmail.com <badnikitosik55@gmail.com>
2024-12-26 14:24:06 +0300

    Добавьте файлы проекта.

---
+++ Tournament_420_Akifev_Nikita_Aleksandrovich/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tournament_420_Akifev_Nikita_Aleksandrovich
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Tournament_420_Akifev_Nikita_Aleksandrovich/Db/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament_420_Akifev_Nikita_Aleksandrovich.Db
+{
+    internal class Class1
+    {
+        public static TournamentDBEntities tournamentDBEntities = new TournamentDBEntities();
+    }
+}
